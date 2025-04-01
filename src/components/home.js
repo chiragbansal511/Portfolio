7c412638dc1492bd08fbaa029d9a3547d035f619
chirag bansal <chirageng.stu22@gmail.com>
2025-04-01 18:26:26 +0530

    navbar : basic structure complete

---
+++ src/components/home.js
@@ -0,0 +1,8 @@
+export default function Home()
+{
+    return (
+        <div className=" bg-slate-700">
+            hello
+        </div>
+    );
+}
---
+++ src/components/navbar.js
@@ -0,0 +1,17 @@
+export default function Navbar()
+{
+    return(
+        <div className=" w-full h-14 flex">
+            <div className=" w-1/3 h-full bg-black flex justify-end text-center">
+            <p className="h-full text-white font-semibold text-2xl flex items-center p-2">Chirag Bansal</p>
+            </div>
+            <div className=" w-2/3 h-full flex items-center">
+            <div className=" h-full text-base font-medium flex justify-center p-3 ml-6 text-slate-400 items-center">Home</div>
+            <div className=" h-full text-base font-medium flex justify-center p-3 ml-6 text-slate-400 items-center">About</div>
+            <div className=" h-full text-base font-medium flex justify-center p-3 ml-6 text-slate-400 items-center">Portfolio</div>
+            <div className=" h-full text-base font-medium flex justify-center p-3 ml-6 text-slate-400 items-center">Contact</div>
+            <div className=" w-1/6 rounded-3xl h-3/4 text-lg font-medium flex justify-center p-3 ml-10 bg-violet-500 items-center text-white">Download CV</div>
+            </div>
+        </div>
+    );
+}
